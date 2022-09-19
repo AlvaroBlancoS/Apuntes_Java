91389ece8cb20eda14bf66bf5998c80fd9e1bb37
Álvaro Blanco Sanginés <35927676+AlvaroBlancoS@users.noreply.github.com>
2022-09-19 22:33:51 +0200

    Otro ejemplo más
    Esta es la parte más sencilla para entender sin utilizar métodos getters y setters

---
+++ introduccionJava/Encapsulacion/src/Vehiculos2/Motor.java
@@ -0,0 +1,24 @@
+package Vehiculos2;
+
+public class Motor {
+	
+	String modelo;
+	int caballos;
+	double parNm;
+	int numCilindros;
+	
+	public Motor() {
+		
+	}
+
+	public Motor(String modelo, int caballos, double parNm, int numCilindros) {
+		super();
+		this.modelo = modelo;
+		this.caballos = caballos;
+		this.parNm = parNm;
+		this.numCilindros = numCilindros;
+	}
+	
+	
+	
+}
