f36d01dad89fa4b07175b09c5e554930bded2a11
Álvaro Blanco Sanginés <35927676+AlvaroBlancoS@users.noreply.github.com>
2022-09-19 22:56:21 +0200

    clase y herencia o polimorfismo
    Aún queda que añadir más ejemplos

--- introduccionJava/HerenciaYpolimorfismo/src/Vehiculos2/Main.java
+++ introduccionJava/HerenciaYpolimorfismo/src/Vehiculos2/Main.java
@@ -1,26 +1,28 @@
 package Vehiculos2;
 
+import HerenciaVehiculos2.Motor;
 import HerenciaVehiculos2.Vehiculo;
 
 public class Main {
 	public static void main(String[] args) {
-
-		// Esto es una herencia
+		// Esto es una clase
+		Motor motorGTI = new Motor("GTI", 190, 459.0, 6);
+		// Puedes usar la herencia
 		Motocicleta kawasakiNinja = new Motocicleta();
 		Coche bmw = new Coche();
 		Camion mercedes = new Camion();
 
-		// Esto es polimorfismo
+		// O polimorfismo
 		Vehiculo vehiculo;
 
-		vehiculo = new Motocicleta();
+		vehiculo = new Motocicleta("Ford", "Mondeo", 2.1, 2010, false, 0, motorGTI, false);
 
 		vehiculo.acelerar(50);
 
 		vehiculo = new Coche();
-		
+
 		vehiculo.acelerar(50);
-		
+
 		vehiculo = new Camion();
 
 		vehiculo.acelerar(50);
