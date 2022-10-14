cab8b499d58e51487791d33a528934694132943e
AlvaroBlancoS <alvaroblancon.sangines.to@gmail.com>
2022-10-14 14:25:21 +0200

    Como funciona las excepciones

---
+++ introduccionJava/Excepciones/src/DivisionFormatException.java
@@ -0,0 +1,13 @@
+
+public class DivisionFormatException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DivisionFormatException(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
---
+++ introduccionJava/Excepciones/src/primerEjemplo/ExceptionDividirUno.java
@@ -0,0 +1,13 @@
+package primerEjemplo;
+
+public class ExceptionDividirUno extends RuntimeException {
+	
+	public ExceptionDividirUno() {
+
+	}
+	
+	public ExceptionDividirUno(String texto) {
+		super(texto);
+	}
+
+}
