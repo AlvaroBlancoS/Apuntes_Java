deabb7031b8f8b2383eae4a3bd0705ac05593259
alvaroblancosangines <alvaroblancones@gmail.com>
2022-12-30 18:32:11 +0100

    Solo habran ejercicios especiales

--- introduccionJava/Excepciones/src/NameFormatException.java
+++
@@ -1,8 +0,0 @@
-
-public class NameFormatException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-	public NameFormatException(String errorMessage) {
-		super(errorMessage);
-	}
-}
