415596a7724478558039db2fb9f575bf6042851a
USERSAD\ablansan <lvaro Blanco>
2025-11-27 16:53:58 +0100

    Cleaning up after the previous project.

--- Conditions/BasicCondition.java
+++
@@ -1,8 +0,0 @@
-public class BasicCondition {
-    public static void main(String[] args) {
-        int edad = 18;
-        if (edad <=18) {
-            System.out.println("Eres mayor de edad");
-        }
-    }
-}
